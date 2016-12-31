a6bc80c79f52ecd504b65d32cc28a1d9f91aa96a
Samuel Mak <samuelswmak@gmail.com>
2016-12-30 19:19:27 -0800

    note test play

---
+++ frontend/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import SynthContainer from './synth/synth_container';
+// import RecorderContainer from './recorder/recorder_container';
+// import JukeboxContainer from './jukebox/jukebox_container';
+
+class App extends React.Component {
+  render () {
+    return (
+      <main>
+        <h1>synth</h1>
+
+      </main>
+    );
+  }
+}
+
+export default App;
+
+// <SynthContainer />
+// <RecorderContainer />
+// <JukeboxContainer />
---
+++ frontend/synthesizer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Note from './util/note';
+import configureStore from './store/store';
+import Root from './components/root';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.hi = "hi"; //test
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+
+  window.store = store; //test
+  console.log(store); //test
+
+  window.Note = Note; //test
+});
